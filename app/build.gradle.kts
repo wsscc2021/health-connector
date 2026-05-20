ced66725b926c8d2cb076671fa417ea49c82e5f8
Donggeun Lee <piou987@gmail.com>
2026-05-20 16:29:45 +0900

    Upgrade gradle version to build apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.ajou.runningcoach.healthconnector"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.ajou.runningcoach.healthconnector"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
