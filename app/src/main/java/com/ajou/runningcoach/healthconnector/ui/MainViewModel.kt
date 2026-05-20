6212afec859aaac58cfdd153c9c3c3ad17d73d59
Donggeun Lee <piou987@gmail.com>
2026-05-20 20:50:02 +0900

    Add library in MainView

--- app/src/main/java/com/ajou/runningcoach/healthconnector/ui/MainViewModel.kt
+++ app/src/main/java/com/ajou/runningcoach/healthconnector/ui/MainViewModel.kt
@@ -18,6 +18,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import java.io.IOException
+import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneOffset
 
